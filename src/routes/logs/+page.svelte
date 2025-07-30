df808b448dbd7c7da6f1036a715d7b8cf878f8b3
TeejMcSteez <teejmcsteez@gmail.com>
2025-07-30 19:50:53 -0400

    feat: added basic data tag to see which day your on

--- src/routes/logs/+page.svelte
+++ src/routes/logs/+page.svelte
@@ -1,10 +1,12 @@
 <script>
     export let data;
     let logs = data.logs;
+    let date = data.selectedDate;
     console.log(logs);
 </script>
 
 <h1>Logs</h1>
+<h2>Date: {date}</h2>
 {#each logs.data as log}
 <div class="m-2 p-2">
     <p>Time: {log.timestamp}</p>
